6b76dd6019da38e76c5117343c5060b26cf86dd7
Jindong Huang <jindongh@google.com>
2022-12-17 19:16:45 -0800

    Add mongodb support

--- app.go
+++ app.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"embed"
+	"time"
+	"context"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //go:embed templates/*
@@ -14,21 +20,67 @@ var resources embed.FS
 
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
+type Receipt struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	Text      string             `bson:"text"`
+}
+
+func getCollection(ctx context.Context) *mongo.Collection {
+	dbUrl := os.Getenv("DB_URL")
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	receiptDatabase := client.Database("receipt")
+	receiptsCollection := receiptDatabase.Collection("receipts")
+	return receiptsCollection 
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 
 	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		text := r.URL.Query().Get("text")
+		collection := getCollection(ctx)
+		_, err := collection.InsertOne(ctx, &Receipt{
+			ID:        primitive.NewObjectID(),
+			CreatedAt: time.Now(),
+			Text:      text,
+		})
+		log.Fatal(err)
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]string{
-			"Region": os.Getenv("FLY_REGION"),
-			"Database": os.Getenv("DB_PASSWORD"),
-			"Env": strings.Join(os.Environ(), "\n"),
+		collection := getCollection(ctx)
+		filter := bson.D{{}}
+		cur, err := collection.Find(ctx, filter)
+		if err != nil {
+			log.Fatal("jindongh")
+			log.Fatal(err)
+		}
+		receipts := []Receipt{}
+		for cur.Next(ctx) {
+			r := Receipt{}
+			_ = cur.Decode(&t)
+			receipts = append(receipts, r)
 		}
 
-		t.ExecuteTemplate(w, "index.html.tmpl", data)
+		t.ExecuteTemplate(w, "index.html.tmpl", receipts)
 	})
 
 	log.Println("listening on", port)
