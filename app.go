e23bc2d14a5cedf1548442fac6914c4d77f22fe6
Jindong Huang <jindongh@google.com>
2022-12-17 17:17:27 -0800

    Create example with CI

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+//go:embed templates/*
+var resources embed.FS
+
+var t = template.Must(template.ParseFS(resources, "templates/*"))
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Region": os.Getenv("FLY_REGION"),
+			"Database": os.Getenv("DB_PASSWORD"),
+			"Env": strings.Join(os.Environ(), "\n"),
+		}
+
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+
+	log.Println("listening on", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
